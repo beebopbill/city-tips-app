9c745f36492e2fc0c18bf995c9fb64e4d84eb351
beebopbill <bill@billmcbride.co>
2023-09-29 13:10:51 -0400

    three

--- scripts.js
+++ scripts.js
@@ -1,3 +1,38 @@
+// Initialize Firebase (replace with your config from Firebase Console)
+var firebaseConfig = {
+    apiKey: "AIzaSyASUiM8JcuQTcaEfsou2FMEYszHj9QZvAE",
+  authDomain: "city-web-app-587d9.firebaseapp.com",
+  databaseURL: "https://city-web-app-587d9-default-rtdb.firebaseio.com",
+  projectId: "city-web-app-587d9",
+  storageBucket: "city-web-app-587d9.appspot.com",
+  messagingSenderId: "1043876965787",
+  appId: "1:1043876965787:web:a9a7036a0ab8300511053e"
+};
+firebase.initializeApp(firebaseConfig);
+
+// Reference to your database
+var database = firebase.database();
+
+// Function to add a tip to the database
+function addTip(city, tip) {
+    var newTipRef = database.ref('tips/' + city).push();
+    newTipRef.set({
+        tip: tip
+    });
+}
+
+// Function to get tips from the database
+function getTips(city, callback) {
+    var tipsRef = database.ref('tips/' + city);
+    tipsRef.on('value', function(snapshot) {
+        var tips = snapshot.val();
+        callback(tips);
+    });
+}
+
+// ... rest of your JavaScript ...
+
+
 let currentCity = '';
 
 function goHome() {
