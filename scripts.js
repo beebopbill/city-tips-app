f517324648e30a5fc16af2122e958d48c4e8b0ea
beebopbill <bill@billmcbride.co>
2023-09-30 04:50:35 -0400

    colors1

--- scripts.js
+++ scripts.js
@@ -7,51 +7,40 @@ const firebaseConfig = {
     storageBucket: "city-web-app-587d9.appspot.com",
     messagingSenderId: "1043876965787",
     appId: "1:1043876965787:web:a9a7036a0ab8300511053e"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-const database = firebase.database();
-
-let currentCity = '';
-
-function showHome() {
-    document.getElementById('home').style.display = 'block';
-    document.getElementById('city-section').style.display = 'none';
-}
-
-function showCity(city) {
-    currentCity = city;
-    document.getElementById('city-name').textContent = city;
-    document.getElementById('home').style.display = 'none';
-    document.getElementById('city-section').style.display = 'block';
-    fetchTips();
-}
-
-function addTipToFirebase() {
-    const tip = document.getElementById('tip-input').value;
-    if (tip) {
-        const newTipRef = database.ref('tips/' + currentCity).push();
-        newTipRef.set({
-            tip: tip
-        });
-        document.getElementById('tip-input').value = '';
-        fetchTips();
-    }
-}
-
-function fetchTips() {
-    const tipsRef = database.ref('tips/' + currentCity);
-    tipsRef.on('value', function(snapshot) {
-        const tips = snapshot.val();
-        const tipsList = document.getElementById('tips-list');
-        tipsList.innerHTML = '';
-        for (let tipId in tips) {
-            const tipItem = document.createElement('div');
-            tipItem.textContent = tips[tipId].tip;
-            tipsList.appendChild(tipItem);
-        }
-    });
-}
-
-showHome();
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.database();
+  
+  const cityTipForm = document.getElementById('cityTipForm');
+  const cityTipsList = document.getElementById('cityTipsList');
+  
+  // Handle form submission
+  cityTipForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+  
+      const city = document.getElementById('citySelect').value;
+      const tip = document.getElementById('cityTip').value;
+  
+      // Save to Firebase
+      db.ref('cityTips').push({
+          city,
+          tip
+      });
+  
+      // Clear the form
+      cityTipForm.reset();
+  });
+  
+  // Listen for changes in the database and populate the list
+  db.ref('cityTips').on('value', (snapshot) => {
+      cityTipsList.innerHTML = ''; // Clear the list
+  
+      snapshot.forEach(childSnapshot => {
+          const data = childSnapshot.val();
+          const li = document.createElement('li');
+          li.textContent = `${data.city}: ${data.tip}`;
+          cityTipsList.appendChild(li);
+      });
+  });
+  
